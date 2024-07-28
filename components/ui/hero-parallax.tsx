17d91c1b4311746a79055d6e18500c8f0726f94c
antho <anthonyyoussef01@gmail.com>
2024-07-28 12:40:56 -0400

    Responsiveness Updates:
    - Updated the responsiveness of the site by adding a max width to the home page paralex text

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -105,7 +105,7 @@ export const HeroParallax = ({
 
 export const Header = () => {
     return (
-        <div className="max-w-7xl relative mx-auto py-5 md:py-10 px-4 w-full  left-0 top-0 z-10">
+        <div className="max-w-[30%] relative mx-auto py-5 md:py-10 px-4 w-full  left-0 top-0 z-10">
             <BlurIn
                 word=""
                 className="h-56 w-56 object-cover rounded-full mb-2 dark:bg-black bg-white"
